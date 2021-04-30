296d9ca7a48c6f8ad2b849acdcdf002c79e0ae49
William Addesso <wsaddesso@gmail.com>
2021-04-30 15:12:16 -0400

    Refactored code that checks for validity. When validity is false, the errorMessage function runs to display the correct error to user. Both password inputs also check for matchin password errors.

--- main.js
+++ main.js
@@ -3,55 +3,53 @@ const country = document.getElementById('country');
 const zip = document.getElementById('zip');
 const password = document.getElementById('password');
 const confirmPassword = document.getElementById('confirm-password');
+const error = document.getElementById('error');
 
 // Checks that user inputs an email address and gives custom error if not.
 email.addEventListener('input', (e) => {
-  if (email.validity.typeMismatch) {
-    email.setCustomValidity('Please enter an email address.');
+  if (email.validity.valid) {
+    error.textContent = '';
   } else {
-    email.setCustomValidity('');
+    errorMessage();
   }
 });
 
 // Checks that the user enters input with letters only.
 country.addEventListener('input', (e) => {
-  if (country.validity.patternMismatch) {
-    country.setCustomValidity('Please enter country names only.');
+  if (country.validity.valid) {
+    error.textContent = '';
   } else {
-    country.setCustomValidity('');
+    errorMessage();
   }
 });
 
 // Checks that the user enters numbers only and a length appropriate for zip codes.
 zip.addEventListener('input', (e) => {
-  if (zip.validity.patternMismatch) {
-    zip.setCustomValidity('Please enter a valid zip code.');
+  if (zip.validity.valid) {
+    error.textContent = '';
   } else {
-    zip.setCustomValidity('');
+    errorMessage();
   }
 });
 
 // Checks input for at least one upper and lower case letter, number, and one symbol.
 // Checks that password matches input in confirm password.
 password.addEventListener('input', (e) => {
-  if (password.validity.patternMismatch) {
-    password.setCustomValidity(
-      'Please enter at least one letter(uppercase and lowercase), one number and one symbol'
-    );
-  } else if (!passwordMatch()) {
-    password.setCustomValidity('Passwords do not match.');
+  if (!passwordMatch()) {
+    errorMessage();
+  } else if (password.validity.valid) {
+    error.textContent = '';
   } else {
-    password.setCustomValidity('');
+    errorMessage();
   }
 });
 
 // Checks taht confirm password matches password.
 confirmPassword.addEventListener('input', (e) => {
   if (passwordMatch()) {
-    confirmPassword.setCustomValidity('');
-    password.setCustomValidity('');
+    error.textContent = '';
   } else {
-    confirmPassword.setCustomValidity('Passwords do not match.');
+    errorMessage();
   }
 });
 
@@ -61,5 +59,21 @@ function passwordMatch() {
     return true;
   } else {
     return false;
+  }
+}
+
+// Displays correct validity message
+function errorMessage() {
+  if (email.validity.typeMismatch) {
+    error.textContent = 'Please enter an email address.';
+  } else if (country.validity.patternMismatch) {
+    error.textContent = 'Please enter country names only.';
+  } else if (zip.validity.patternMismatch) {
+    error.textContent = 'Please enter a valid zip code.';
+  } else if (password.validity.patternMismatch) {
+    error.textContent =
+      'Please enter at least one letter(uppercase and lowercase), one number and one symbol';
+  } else if (!passwordMatch()) {
+    error.textContent = 'Passwords do not match.';
   }
 }
